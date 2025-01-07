bf43f4735d989cd9f040c9bbfeed41cca72fa562
William Kim <geminiwing@users.noreply.github.com>
2025-01-07 10:55:36 -0800

    Add type defs

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// import type definitions
+import { typeDefs } from './schema.js';
+
 // Set up apollo server
 const server = new ApolloServer({
+  typeDefs,
 });
 
 const { url } = await startStandaloneServer(server, {
---
+++ schema.js
@@ -0,0 +1,28 @@
+// Supported scalar types: Int, Float, String, Boolean, and ID.
+// Exclamation (!) idicates that the field is required.
+//
+// For example, [String!]! indicates that the field is an array of strings that
+// cannot be null or empty.
+
+export const typeDefs = `#graphql
+  type Game {
+    id: ID!
+    name: String!
+    platform: [String!]!
+  }
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+  }
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+  }
+  type Query {
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+  }
+`
